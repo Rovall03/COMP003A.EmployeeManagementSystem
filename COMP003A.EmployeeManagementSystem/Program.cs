a54d1eb95e4c4fd44c0ef73ee97f7df1a593e280
rosa ovalle <you@example.com>
2025-02-14 20:59:00 -0800

    main

--- COMP003A.EmployeeManagementSystem/Program.cs
+++ COMP003A.EmployeeManagementSystem/Program.cs
@@ -7,14 +7,30 @@ namespace COMP003A.EmployeeManagementSystem
 
         static void Main(string[] args)
         {
-           
+            string employeeId = "E001";
+          
+            string firstName = " Alice";
+            string middleName = "Marie";
+            string lastName = "Johnson";
+            double salary =75000;
+
+
+
+            Employee employee = new Employee(employeeId, firstName, middleName,lastName,salary);
+            employee.DisplayEmployeeInfo(); 
    
        
+            Console.WriteLine("Employee Created Successfully!");
+
+
        HRDepartment hr =new HRDepartment();
-            ITDepartment it = new ITDepartment();
+            
 
             hr.DisplayDepartmentInfo();
             hr.Operate();
+
+
+ITDepartment it = new ITDepartment();
             it.DisplayDepartmentInfo();
             it.Operate();
 
